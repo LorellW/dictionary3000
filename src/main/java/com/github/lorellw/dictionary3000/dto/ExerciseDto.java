3c3920dc70c72db25c4f69e82ae5ba2cab5b435c
LorellW <lorellwendel@gmail.com>
2023-06-01 11:26:48 +0300

    6/1/23
    added ExercideDto

---
+++ src/main/java/com/github/lorellw/dictionary3000/dto/ExerciseDto.java
@@ -0,0 +1,17 @@
+package com.github.lorellw.dictionary3000.dto;
+
+import com.github.lorellw.dictionary3000.entities.GrammaticalTask;
+import com.github.lorellw.dictionary3000.entities.Lesson;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExerciseDto {
+    private int seqNumb;
+    private String text;
+
+}
---
+++ src/main/java/com/github/lorellw/dictionary3000/dto/GrammaticalTaskDto.java
@@ -0,0 +1,11 @@
+package com.github.lorellw.dictionary3000.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class GrammaticalTaskDto {
+    private String taskText;
+    private String answer;
+}
